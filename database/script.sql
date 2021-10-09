53a0c8619f8ccc59ea8e0b865f84e98abd72f080
edenroseFR <edenroserisma13@gmail.com>
2021-10-10 03:27:42 +0800

    Replace .gitignore content with the flask-generated .gitignore content

---
+++ database/script.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE SSISdb;
+USE SSISdb;
+
+CREATE TABLE `course`(
+code VARCHAR(10) NOT NULL,
+name VARCHAR(50) NOT NULL,
+PRIMARY KEY(code)
+);
+CREATE TABLE `college`(
+code VARCHAR(10) NOT NULL,
+name VARCHAR(50) NOT NULL,
+PRIMARY KEY(code)
+);
+
+CREATE TABLE `students`(
+id VARCHAR(9) NOT NULL,
+firstname VARCHAR(50) NOT NULL,
+middlename VARCHAR(20) NOT NULL,
+lastname VARCHAR(20) NOT NULL,
+year INT(1) NOT NULL,
+gender VARCHAR(10) NOT NULL,
+coursecode VARCHAR(10) NOT NULL,
+collegecode VARCHAR(10),
+PRIMARY KEY(id),
+FOREIGN KEY(coursecode) REFERENCES course(code),
+FOREIGN KEY(collegecode) REFERENCES college(code)
+);
+
