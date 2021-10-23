02dbc5afce2fce76fe065c3f984048eecfb0e3be
edenroseFR <edenroserisma13@gmail.com>
2021-10-24 02:01:36 +0800

    Dump course and college rows

--- db_script/script.sql
+++ db_script/script.sql
@@ -7,12 +7,16 @@ password VARCHAR(100) NOT NULL,
 PRIMARY KEY(username)
 );
 
+INSERT INTO `admin`(`username`, `password`)
+VALUES ('eden', 'rose');
+
 CREATE TABLE `course`(
 code VARCHAR(10) NOT NULL,
 name VARCHAR(50) NOT NULL,
 college VARCHAR(45) NOT NULL,
 PRIMARY KEY(code)
 );
+
 CREATE TABLE `college`(
 code VARCHAR(10) NOT NULL,
 name VARCHAR(50) NOT NULL,
@@ -34,3 +38,17 @@ FOREIGN KEY(coursecode) REFERENCES course(code),
 FOREIGN KEY(collegecode) REFERENCES college(code)
 );
 
+INSERT INTO `college`(`code`, `name`)
+VALUES ('CCS', 'College of Computer Studies'),
+	   ('CSM', 'College of Science and Mathematics'),
+       ('CASS', 'College of Arts and Social Sciences'),
+       ('COET', 'College of Engineering and Technology'),
+       ('CED', 'College of Education'),
+       ('CON', 'College of Nursing');
+
+INSERT INTO `course`(`code`, `name`, `college`)
+VALUES ('BSCS', 'Bachelor of Science in Computer Science', 'CCS'),
+	   ('BSIT', 'Bachelor of Science in Information Technology', 'CCS'),
+       ('BSIS', 'Bachelor of Science in Information System', 'CCS'),
+       ('BSCE', 'Bachelor of Science in Civil Engineering', 'COET'),
+       ('BSChemEng', 'Bachelor of Science in Chemical Engineering', 'CCS');
