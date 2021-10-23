94ec692560a1020de6f54a932777d4f5e6bc0ac0
edenroseFR <edenroserisma13@gmail.com>
2021-10-24 02:10:32 +0800

    Replace slash in save_image directory

--- ssis/models/__init__.py
+++ ssis/models/__init__.py
@@ -1,11 +1,17 @@
 import mysql.connector as mysql
 
 
+# db = mysql.connect(
+#             host = 'us-cdbr-east-04.cleardb.com',
+#             user = 'b0b5e5ffe03874',
+#             password = '971dad9f',
+#             database = 'heroku_7cab8ea6c6ec62e'
+#         )
 db = mysql.connect(
-            host = 'us-cdbr-east-04.cleardb.com',
-            user = 'b0b5e5ffe03874',
-            password = '971dad9f',
-            database = 'heroku_7cab8ea6c6ec62e'
+            host = 'localhost',
+            user = 'root',
+            password = 'edenrose',
+            database = 'ssisdb'
         )
 cursor = db.cursor()
 
