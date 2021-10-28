909e5897bde4f5b18039a50293bfb0245db269b9
edenroseFR <edenroserisma13@gmail.com>
2021-10-28 13:14:34 +0800

    Add dotenv

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from ssis import create_app
+from dotenv import load_dotenv
 
 app = create_app()
+
+load_dotenv('.env')
 
 if __name__ == '__main__':
     app.run()
--- ssis/models/__init__.py
+++ ssis/models/__init__.py
@@ -1,17 +1,11 @@
 import mysql.connector as mysql
-
+from os import getenv
 
 db = mysql.connect(
-            host = 'us-cdbr-east-04.cleardb.com',
-            user = 'b0b5e5ffe03874',
-            password = '971dad9f',
-            database = 'heroku_7cab8ea6c6ec62e'
+            host = getenv('DB_HOST'),
+            user = getenv('DB_USERNAME'),
+            password = getenv('DB_PASSWORD'),
+            database = getenv('DB_NAME')
         )
-# db = mysql.connect(
-#             host = 'localhost',
-#             user = 'root',
-#             password = 'edenrose',
-#             database = 'ssisdb'
-#         )
 cursor = db.cursor()
 
