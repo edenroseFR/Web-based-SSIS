5d89e0ddfde82570cfca10ddfd32f2e0104c8e76
edenroseFR <edenroserisma13@gmail.com>
2021-11-01 20:22:06 +0800

    Remove account creation for admin

--- ssis/views/admin/routes.py
+++ ssis/views/admin/routes.py
@@ -10,18 +10,3 @@ def login() -> str:
         redirect(url_for('homepage'))
     return render_template('index.html')
 
-
-@admin.route('/admin/signup')
-def signup() -> str:
-    return render_template('signup.html')
-
-
-@admin.route('/admin/confirm_identity', methods=['GET', 'POST'])
-def confirm_identity() -> str:
-    username = request.form.get('username')
-    password = request.form.get('password')
-    password2 = request.form.get('passwordConfirmation')
-
-    if verified(username, password, password2):
-        return redirect(url_for('homepage'))
-    return render_template('signup.html')
