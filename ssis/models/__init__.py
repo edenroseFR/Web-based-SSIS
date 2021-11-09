b6e6b60389cb58c36088441f52058fef627e07dc
edenroseFR <edenroserisma13@gmail.com>
2021-11-09 20:37:50 +0800

    Change db connection for heroku deployment

--- ssis/models/__init__.py
+++ ssis/models/__init__.py
@@ -2,10 +2,14 @@ import mysql.connector as mysql
 from os import getenv
 
 db = mysql.connect(
-            host = getenv('DB_HOST'),
-            user = getenv('DB_USERNAME'),
-            password = getenv('DB_PASSWORD'),
-            database = getenv('DB_NAME')
+            # host = getenv('DB_HOST'),
+            # user = getenv('DB_USERNAME'),
+            # password = getenv('DB_PASSWORD'),
+            # database = getenv('DB_NAME')
+            host = 'us-cdbr-east-04.cleardb.com',
+            user = 'b0b5e5ffe03874',
+            password = '971dad9f',
+            database = 'heroku_7cab8ea6c6ec62e'
         )
 cursor = db.cursor()
 
