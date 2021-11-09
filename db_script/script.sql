42d36ccb008f225b7ee73954c79b397fafbbbc8d
edenroseFR <edenroserisma13@gmail.com>
2021-11-09 20:27:55 +0800

    Change script for heroku deployment

--- db_script/script.sql
+++ db_script/script.sql
@@ -8,7 +8,7 @@ PRIMARY KEY(username)
 );
 
 INSERT INTO `admin`(`username`, `password`)
-VALUES ('eden', 'rose');
+VALUES ('eden', 'sha256$ULUS96W3djTrRi7e$233ff68eb2b2c3593440e1d6c0dabaaea17a9f246328db4ab1f09d05f5929568');
 
 CREATE TABLE `course`(
 code VARCHAR(10) NOT NULL,
